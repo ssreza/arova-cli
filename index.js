9ebd7492a85b01feae79be679a842e6c1af89a64
shifat <shifat@primoplatform.com>
2016-10-15 22:54:41 -0400

    tested

--- index.js
+++ index.js
@@ -30,24 +30,40 @@ $('#ledSet').on('click',function(){
 $("#up").on('click', function(){
   moving = true ;
 
-  socket.emit('motor',{command:"up", speed:120});
+  socket.emit('motor',{command:"up", speed:20});
 });
 
+
+$("#down").on('click', function(){
+  moving = true ;
+
+  socket.emit('motor',{command:"down", speed:20});
+});
+
+$("#right").on('click', function(){
+
+  socket.emit('direction',{command:"right"});
+});
+
+$("#left").on('click', function(){
+
+  socket.emit('direction',{command:"left"});
+});
 $("#brake").on('click', function(){
- 
-  if($(this).text() === 'Stop'){
-     moving = false;
-      $(this).text("Resume");
-      socket.emit('motor',{command:"brake"});
-
-    }else if($(this).text() === 'Resume'){
-      $(this).text("Stop");
-      socket.emit('motor',{command:"resume"});
-    }
+  
+  socket.emit('motor',{command:"brake"});
 });
 
-var speed = 128;
+
+var speed = 20;
 document.addEventListener('keydown', function(event) {
+
+  if(event.keyCode == 87) {
+        
+        socket.emit('motor',{command:"up",speed });
+    }
+
+
     if(event.keyCode == 38 && moving ===true) {
         if(speed+31 < 255)
           {speed+=31;}
@@ -56,7 +72,7 @@ document.addEventListener('keydown', function(event) {
         }
         socket.emit('motor',{command:"speedUp",speed });
     }
-    else if(event.keyCode == 40 && moving ===true) {
+     if(event.keyCode == 40 && moving ===true) {
       if(speed-31 >= 0)
           {speed-=31;}else{
             speed = 0;
@@ -66,13 +82,24 @@ document.addEventListener('keydown', function(event) {
         console.log('slowing');
     }
 
-    else if(event.keyCode == 40 && moving ===true) {
-      if(speed-31 >= 0)
-          {speed-=31;}else{
-            speed = 0;
-          }
+    if(event.keyCode == 83 ) {
+      if(speed+31 < 255)
+          {speed+=31;}
+        else  {
+          speed = 255;
+        }
 
-        socket.emit('motor',{command:"slowDown",speed });
-        console.log('Down was pressed');
+        socket.emit('motor',{command:"down",speed });
+        moving = true;
+        console.log('slowing');
     }
+
+    if(event.keyCode == 32 && moving ===true) {
+        moving= false;
+        socket.emit('motor',{command:"brake"});
+        console.log('Down was pressed');
+        $('#brake').text("Resume");
+
+    }
+
 });
--- test.js
+++ test.js
@@ -3,8 +3,26 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
   , five = require("johnny-five"),
-  board,servo,led,sensor, motor;
+  board,servo,led,sensor, motor, direction, position;
+ 
 
+   var fs = require("fs");
+
+function readJson(){
+   var contents = fs.readFileSync("position.json");
+// Define to JSON type
+  position = JSON.parse(contents);
+}
+
+function writeJson(){
+  var str = JSON.stringify(position);
+  fs.writeFile('position.json', str, function (err) {
+    if (err) return console.log(err);
+    console.log(str + ' > position.json');
+  });
+}
+
+readJson();
 board = new five.Board();
 
 // on board ready
@@ -12,6 +30,7 @@ board.on("ready", function() {
 
   // init a led on pin 13, strobe every 1000ms
   led = new five.Led(13).strobe(1000);
+
   motor = new five.Motor({
     pins: {
       pwm: 3,
@@ -20,19 +39,14 @@ board.on("ready", function() {
     }
   });
 
-  // motor.on("forward", function(err, timestamp) {
-  //   // demonstrate braking after 5 seconds
-  //   board.wait(5000, function() {
-  //     motor.brake();
-  //   });
-  // });
-
-   motor.on("brake", function(err, timestamp) {
-    // Release the brake after .1 seconds
-    board.wait(100, function() {
-      motor.stop();
-    });
+   direction = new five.Motor({
+    pins: {
+      pwm: 5,
+      dir: 8,
+      brake: 10
+    }
   });
+ 
   // setup a stanard servo, center at start
   servo = new five.Servo({
     pin:6,
@@ -75,7 +89,7 @@ io.sockets.on('connection', function (socket) {
   // if board is ready
   if(board.isReady){
     // read in sensor data, pass to browser
-     socket.emit('news', { message: 'board is ready' });
+     socket.emit('news', { message: 'board is ready', position:position });
     sensor.on("data",function(){
 
       socket.emit('sensor', { raw: this.raw });
@@ -92,17 +106,68 @@ io.sockets.on('connection', function (socket) {
     console.log(data);
      if(board.isReady){    led.strobe(data.delay); } 
   });
+var count =0;  
+
+
+ socket.on('direction', function(data){
+  if (data.command) {
+    if (data.command === 'left') {
+      if(board.isReady){    
+        
+         var left= setInterval(function(){
+                    direction.forward(255);
+                    
+                 }, 50); 
+          setTimeout(function(){
+            clearInterval(left);
+            position.angle += 500;
+            writeJson(position);
+          }, 1000 - position.angle);
+       
+      } 
+
+    }
+   if (data.command === 'brake') {
+         if(board.isReady){    motor.brake(); } 
+
+        }
+    if (data.command === 'right') {
+      if(board.isReady){   
+        var right= setInterval(function(){
+                    direction.reverse(255);
+                    
+                 }, 50); 
+        setTimeout(function() {
+          clearInterval(right)
+          position.angle -= 500;
+            writeJson(position);
+        }, 1000 + position.angle);
+      } 
+
+    }
+  }
+  data.position = position;
+  socket.emit('motorMsg', { raw: data });
+ });
  socket.on('motor', function (data) {
     //if(board.isReady){ motor.to(data.pos);  }
     if (data.command ) {
-      var speed = data.speed || 128;
+      var speed = data.speed || 20;
       if (data.command === 'up') {
        if(board.isReady){    motor.forward(speed); } 
 
       }
 
+       if (data.command === 'down') {
+       if(board.isReady){    
+
+        motor.reverse(speed); 
+      } 
+
+      }
+
       if (data.command === 'brake') {
-       if(board.isReady){    motor.brake(); } 
+       if(board.isReady){    motor.brake(); motor.stop() } 
 
       }
 
