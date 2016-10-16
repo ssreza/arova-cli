e9e95a2f69f881e93600ff925779c8653a69312c
Faraz Fazli <farazfazli@gmail.com>
2016-10-16 02:40:11 -0400

    hello

--- index.js
+++ index.js
@@ -1,105 +1,149 @@
-
 var moving = false;
 var socket = io.connect('http://localhost:3000');
-socket.on('news', function (data) {
-  console.log(data);
+socket.on('news', function(data) {
+    console.log(data);
 });
-socket.on('motorMsg', function (data) {
-  console.log(data.raw);
+socket.on('motorMsg', function(data) {
+    console.log(data.raw);
 });
-socket.on('sensor', function (data) {
-  console.log("Incoming sensor data:",data.raw);
-  $("#inData").append(data.raw+"\r");
-  $("#inData").animate({scrollTop:$("#inData")[0].scrollHeight - $("#inData").height()},200);
+socket.on('sensor', function(data) {
+    console.log("Incoming sensor data:", data.raw);
+    $("#inData").append(data.raw + "\r");
+    $("#inData").animate({
+        scrollTop: $("#inData")[0].scrollHeight - $("#inData").height()
+    }, 200);
 
 });
 
 $('.servobtn').button();
 
-$('.servobtn').on('change',function(){
-    console.log("Setting Servo Pos:",$('input[name=servo]:checked').val())
-    socket.emit('servo',{pos:$('input[name=servo]:checked').val()});
+$('.servobtn').on('change', function() {
+    console.log("Setting Servo Pos:", $('input[name=servo]:checked').val())
+    socket.emit('servo', {
+        pos: $('input[name=servo]:checked').val()
+    });
 });
 
-$('#ledSet').on('click',function(){
-  var tmp = parseInt($('#ledDelay').val(),10);
-  console.log("Setting LED Delay:",tmp)
-  socket.emit('led',{delay:tmp});
-});
-
-$("#up").on('click', function(){
-  moving = true ;
-
-  socket.emit('motor',{command:"up", speed:20});
+$('#ledSet').on('click', function() {
+    var tmp = parseInt($('#ledDelay').val(), 10);
+    console.log("Setting LED Delay:", tmp)
+    socket.emit('led', {
+        delay: tmp
+    });
 });
 
 
-$("#down").on('click', function(){
-  moving = true ;
 
-  socket.emit('motor',{command:"down", speed:20});
+$("#right").on('click', function() {
+
+    socket.emit('direction', {
+        command: "right"
+    });
 });
 
-$("#right").on('click', function(){
+$("#left").on('click', function() {
 
-  socket.emit('direction',{command:"right"});
+    socket.emit('direction', {
+        command: "left"
+    });
 });
 
-$("#left").on('click', function(){
-
-  socket.emit('direction',{command:"left"});
-});
-$("#brake").on('click', function(){
-  
-  socket.emit('motor',{command:"brake"});
-});
 
 
 var speed = 20;
 document.addEventListener('keydown', function(event) {
 
-  if(event.keyCode == 87) {
-        
-        socket.emit('motor',{command:"up",speed });
+    if (event.keyCode === 87) {
+
+        socket.emit('motor', {
+            command: "up"
+        });
     }
 
 
-    if(event.keyCode == 38 && moving ===true) {
-        if(speed+31 < 255)
-          {speed+=31;}
-        else  {
-          speed = 255;
+    if (event.keyCode == 38) {
+        if (speed + 31 < 255) {
+            speed += 31;
+        } else {
+            speed = 255;
         }
-        socket.emit('motor',{command:"speedUp",speed });
+        socket.emit('motor', {
+            command: "speedUp",
+            speed
+        });
     }
-     if(event.keyCode == 40 && moving ===true) {
-      if(speed-31 >= 0)
-          {speed-=31;}else{
+    if (event.keyCode == 40) {
+        if (speed - 31 >= 0) {
+            speed -= 31;
+        } else {
             speed = 0;
-          }
-
-        socket.emit('motor',{command:"slowDown",speed });
-        console.log('slowing');
-    }
-
-    if(event.keyCode == 83 ) {
-      if(speed+31 < 255)
-          {speed+=31;}
-        else  {
-          speed = 255;
         }
 
-        socket.emit('motor',{command:"down",speed });
-        moving = true;
+        socket.emit('motor', {
+            command: "slowDown",
+            speed
+        });
         console.log('slowing');
     }
 
-    if(event.keyCode == 32 && moving ===true) {
-        moving= false;
-        socket.emit('motor',{command:"brake"});
-        console.log('Down was pressed');
-        $('#brake').text("Resume");
+    if (event.keyCode == 83) {
+
+
+        socket.emit('motor', {
+            command: "down"
+        });
+        moving = true;
+        console.log('back');
+    }
+
+    if (event.keyCode == 65) {
+
+
+        socket.emit('direction', {
+            command: "left"
+        });
+
+    }
+
+    if (event.keyCode == 68) {
+
+
+        socket.emit('direction', {
+            command: "right"
+        });
+
+    }
+
+    if (event.keyCode == 32) {
+
+        socket.emit('motor', {
+            command: "brake"
+        });
+
+
+        console.log('space was pressed');
 
     }
 
 });
+
+
+document.addEventListener('keyup', function(event) {
+    console.log(event.keyCode);
+    if (event.keyCode === 87) {
+        socket.emit('motor', {
+            command: 'brake'
+        });
+    }
+
+    if (event.keyCode == 83) {
+
+
+        socket.emit('motor', {
+            command: "brake"
+        });
+
+    }
+});
+
+
--- test.js
+++ test.js
@@ -1,65 +1,66 @@
 // initialize everything, web server, socket.io, filesystem, johnny-five
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
-  , five = require("johnny-five"),
-  board,servo,led,sensor, motor, direction, position;
- 
+var app = require('http').createServer(handler),
+    path = require("path"),
+    io = require('socket.io').listen(app),
+    fs = require('fs'),
+    five = require("johnny-five"),
+    board, servo, led, sensor, motor, direction, position;
 
-   var fs = require("fs");
 
-function readJson(){
-   var contents = fs.readFileSync("position.json");
-// Define to JSON type
-  position = JSON.parse(contents);
+var fs = require("fs");
+
+function readJson() {
+
 }
 
-function writeJson(){
-  var str = JSON.stringify(position);
-  fs.writeFile('position.json', str, function (err) {
-    if (err) return console.log(err);
-    console.log(str + ' > position.json');
-  });
+function writeJson() {
+    var str = JSON.stringify(position);
+    fs.writeFile('position.json', str, function(err) {
+        if (err) return console.log(err);
+        console.log(str + ' > position.json');
+    });
 }
 
-readJson();
+fs.readFile("position.json", function(err, contents) {
+    position = JSON.parse(contents)
+});
 board = new five.Board();
 
 // on board ready
 board.on("ready", function() {
 
-  // init a led on pin 13, strobe every 1000ms
-  led = new five.Led(13).strobe(1000);
+    // init a led on pin 13, strobe every 1000ms
+    led = new five.Led(13).strobe(1000);
 
-  motor = new five.Motor({
-    pins: {
-      pwm: 3,
-      dir: 12,
-      brake: 9
-    }
-  });
+    motor = new five.Motor({
+        pins: {
+            pwm: 3,
+            dir: 12,
+            brake: 9
+        }
+    });
 
-   direction = new five.Motor({
-    pins: {
-      pwm: 5,
-      dir: 8,
-      brake: 10
-    }
-  });
- 
-  // setup a stanard servo, center at start
-  servo = new five.Servo({
-    pin:6,
-    range: [0,180],
-    type: "standard",
-    center:true
-  });
+    direction = new five.Motor({
+        pins: {
+            pwm: 5,
+            dir: 8,
+            brake: 10
+        }
+    });
 
-  // poll this sensor every second
-  sensor = new five.Sensor({
-    pin: "A0",
-    freq: 1000
-  });
+    // setup a stanard servo, center at start
+    servo = new five.Servo({
+        pin: 6,
+        range: [0, 180],
+        type: "standard",
+        center: true
+    });
+
+    // poll this sensor every second
+    sensor = new five.Sensor({
+        pin: "A0",
+        freq: 1000
+    });
 
 });
 
@@ -68,119 +69,181 @@ app.listen(3000);
 
 
 // handle web server
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+function handler(req, res) {
+    fs.readFile(__dirname + '/index.html', function(err, data) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
 
-    res.writeHead(200);
-    res.end(data);
-  });
+        res.writeHead(200);
+        res.end(data);
+    });
 }
 
 
 // on a socket connection
-io.sockets.on('connection', function (socket) {
- 
- 
-  // if board is ready
-  if(board.isReady){
-    // read in sensor data, pass to browser
-     socket.emit('news', { message: 'board is ready', position:position });
-    sensor.on("data",function(){
-
-      socket.emit('sensor', { raw: this.raw });
-    });
-  }
-
-  // if servo message received
-  socket.on('servo', function (data) {
-    console.log(data);
-    if(board.isReady){ servo.to(data.pos);  }
-  });
-  // if led message received
-  socket.on('led', function (data) {
-    console.log(data);
-     if(board.isReady){    led.strobe(data.delay); } 
-  });
-var count =0;  
+io.sockets.on('connection', function(socket) {
 
 
- socket.on('direction', function(data){
-  if (data.command) {
-    if (data.command === 'left') {
-      if(board.isReady){    
-        
-         var left= setInterval(function(){
-                    direction.forward(255);
-                    
-                 }, 50); 
-          setTimeout(function(){
-            clearInterval(left);
-            position.angle += 500;
-            writeJson(position);
-          }, 1000 - position.angle);
-       
-      } 
+    // if board is ready
+    if (board.isReady) {
+        // read in sensor data, pass to browser
+        socket.emit('news', {
+            message: 'board is ready',
+            position: position
+        });
+        sensor.on("data", function() {
 
+            socket.emit('sensor', {
+                raw: this.raw
+            });
+        });
     }
-   if (data.command === 'brake') {
-         if(board.isReady){    motor.brake(); } 
 
+    // if servo message received
+    socket.on('servo', function(data) {
+        console.log(data);
+        if (board.isReady) {
+            servo.to(data.pos);
         }
-    if (data.command === 'right') {
-      if(board.isReady){   
-        var right= setInterval(function(){
-                    direction.reverse(255);
-                    
-                 }, 50); 
-        setTimeout(function() {
-          clearInterval(right)
-          position.angle -= 500;
-            writeJson(position);
-        }, 1000 + position.angle);
-      } 
+    });
+    // if led message received
+    socket.on('led', function(data) {
+        console.log(data);
+        if (board.isReady) {
+            led.strobe(data.delay);
+        }
+    });
+    var count = 0;
 
-    }
-  }
-  data.position = position;
-  socket.emit('motorMsg', { raw: data });
- });
- socket.on('motor', function (data) {
-    //if(board.isReady){ motor.to(data.pos);  }
-    if (data.command ) {
-      var speed = data.speed || 20;
-      if (data.command === 'up') {
-       if(board.isReady){    motor.forward(speed); } 
 
-      }
+    socket.on('direction', function(data) {
+        if (data.command) {
+            if (data.command === 'left') {
+                if (board.isReady) {
+                    fs.readFile("position.json", function(err, contents) {
+                        position = JSON.parse(contents)
+                        if (position.angle < 1000) {
 
-       if (data.command === 'down') {
-       if(board.isReady){    
+                            var left = setInterval(function() {
+                                direction.forward(255);
 
-        motor.reverse(speed); 
-      } 
+                            }, 50);
+                            setTimeout(function() {
+                                clearInterval(left);
 
-      }
 
-      if (data.command === 'brake') {
-       if(board.isReady){    motor.brake(); motor.stop() } 
+                            }, 1000 - position.angle);
+                            position.angle += 500;
+                            writeJson(position);
 
-      }
+                        }
+                        socket.emit('motorMsg', {
+                            raw: position
+                        });
+                    });
 
-      if (data.command === 'resume') {
-             if(board.isReady){    motor.release(); } 
+
+
+                }
 
             }
-      if (data.command === 'speedUp'){
-               if(board.isReady){    motor.start(speed); } 
+            if (data.command === 'brake') {
+                if (board.isReady) {
+                    motor.brake();
+                }
 
-      }
+            }
+            if (data.command === 'right') {
+                if (board.isReady) {
+                    fs.readFile("position.json", function(err, contents) {
+                        position = JSON.parse(contents)
+                        if (position.angle > -1000) {
 
-    }
-    socket.emit('motorMsg', { raw: data });
-  });
+                            var diff = 1000 + position.angle;
+
+                            var right = setInterval(function() {
+                                direction.reverse(255);
+
+                            }, 50);
+                            setTimeout(function() {
+                                clearInterval(right)
+
+
+                            }, diff);
+                            console.log(diff);
+                            position.angle -= 500;
+                            writeJson(position);
+                        }
+                        socket.emit('motorMsg', {
+                            raw: position
+                        });
+                    });
+
+
+                }
+
+            }
+        }
+
+    });
+    socket.on('motor', function(data) {
+        //if(board.isReady){ motor.to(data.pos);  }
+        if (data.command) {
+            var speed = data.speed || 50;
+
+            data.position = position;
+            if (data.command === 'up') {
+                if (board.isReady) {
+                    position.speed = 50 ;
+                    writeJson(position)
+                    motor.forward(speed);
+                }
+
+            }
+
+            if (data.command === 'down') {
+                if (board.isReady) {
+                    position.speed = 50 ;
+                    writeJson(position)
+                    motor.reverse(speed);
+                }
+
+            }
+
+            if (data.command === 'brake') {
+                if (board.isReady) {
+                    motor.brake();
+                    motor.stop();
+                }
+
+            }
+
+            if (data.command === 'resume') {
+                if (board.isReady) {
+                    motor.release();
+                }
+
+            }
+            if (data.command === 'speedUp') {
+                if (board.isReady) {
+                    motor.start(speed);
+                }
+
+            }
+
+            if (data.command === 'slowDown') {
+                if (board.isReady) {
+                    motor.start(speed);
+                }
+
+
+            }
+
+        }
+        socket.emit('motorMsg', {
+            raw: data
+        });
+    });
 });
